b64b98d844a96fdc0483d6ee9bce7a3e749fd0bb
Kenneth Ng <145921389+kennething@users.noreply.github.com>
2026-04-18 14:40:41 -0400

    commit the db (gone wrong) (cops called)

--- server/utils/sqlite.ts
+++ server/utils/sqlite.ts
@@ -1,4 +1,4 @@
 import Database from "better-sqlite3";
 
-export const db = new Database("./server/db.sqlite3");
+export const db = new Database(`./server/${process.env.NODE_ENV === "production" ? "prod" : "dev"}.sqlite3`);
 db.pragma("journal_mode = WAL");
