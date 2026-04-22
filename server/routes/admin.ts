8e908bedc423a613cd286db10ce24fde9b2fb8a8
Kenneth Ng <145921389+kennething@users.noreply.github.com>
2026-04-22 19:11:55 -0400

    what

--- server/routes/admin.ts
+++ server/routes/admin.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import path from "path";
 
 const router = Router();
 
@@ -6,7 +7,8 @@ router.get("/admin/get-db/:password", async (req, res) => {
   const password = decodeURIComponent(req.params.password);
   if (password !== process.env.ADMIN_PASSWORD) return res.sendStatus(404);
 
-  res.sendFile("server/prod.sqlite3", { root: process.cwd() });
+  console.log(process.cwd());
+  res.sendFile("prod.sqlite3", { root: path.join(process.cwd(), "server") });
 });
 
 export default router;
