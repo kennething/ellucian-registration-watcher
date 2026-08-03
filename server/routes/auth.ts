26b87a6f4978e1a9904b9c195f590fddef54b82a
Kenneth Ng <145921389+kennething@users.noreply.github.com>
2026-08-03 04:27:18 -0400

    style

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -5,6 +5,7 @@ import { Router } from "express";
 import jwt from "jsonwebtoken";
 import axios from "axios";
 import { CLIENT } from "../../bot/src/common";
+import { authController } from "../controllers/auth";
 
 const router = Router();
 
@@ -45,21 +46,47 @@ router.get("/auth/discord/callback", async (req, res) => {
 
     const discordId = (await axios.get("https://discord.com/api/users/@me", { headers: { Authorization: `Bearer ${token}` } })).data!.id;
 
-    const [result, error] = tryCatch<{ uuid: string }>(() => db.prepare("SELECT uuid FROM users WHERE discord_id = ?").get(discordId) as any);
+    const [existingUser, error] = tryCatch<{ uuid: string }>(() => db.prepare("SELECT uuid FROM users WHERE discord_id = ?").get(discordId) as any);
     if (error) return res.sendStatus(500);
 
-    const uuid = result ? result.uuid : uuidv6();
-    if (!result) db.prepare("INSERT INTO users (uuid, discord_id) VALUES (?, ?)").run(uuid, discordId);
+    const uuid = existingUser ? existingUser.uuid : uuidv6();
+    if (!existingUser) db.prepare("INSERT INTO users (uuid, discord_id) VALUES (?, ?)").run(uuid, discordId);
 
     const jwtToken = jwt.sign({ uuid, discordId }, process.env.JWT_SECRET as string, { expiresIn: "7d" });
     res.cookie("token", jwtToken, { httpOnly: true, secure: process.env.NODE_ENV === "production", sameSite: process.env.NODE_ENV === "production" ? "none" : "lax" });
 
     res.redirect(`${process.env.FRONTEND_URL}/watch`);
 
-    const user = await CLIENT.client?.users.fetch(discordId);
-    user?.send(
-      `Thanks for using [Bad Scheduler](<${process.env.FRONTEND_URL}>)! To get the most out of the bot, make sure you:\n1. **Allow Discord notifications** in your system settings\n2. **DON'T mute** this DM channel\n3. **DON'T set Do Not Disturb**\\* as your Discord status\n\n\\*If you have Do Not Disturb enabled, you will not receive Discord push notifications while a.) Discord is open on another device, and/or b.) for a few minutes after you open Discord on your phone`
+    if (!existingUser) {
+      const user = await CLIENT.client?.users.fetch(discordId);
+      user?.send(
+        `## Thanks for using [Bad Scheduler](<${process.env.FRONTEND_URL}>) :)\nTo get the most out of the bot, make sure you:\n\n1. **Allow Discord notifications** in your system settings,\n2. **DON'T mute** this DM channel, and\n3. **DON'T set __Do Not Disturb__** as your Discord status\\*\n\n-# \\*If you have __Do Not Disturb__ enabled, you will not receive Discord push notifications while:\n-# a. Discord is open on another device, and/or\n-# b. for a few minutes after opening Discord on any device`
+      );
+    }
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
+
+router.get("/auth/logout", authController, async (req, res) => {
+  try {
+    await axios.post(
+      "https://discord.com/api/oauth2/token/revoke",
+      new URLSearchParams({
+        token: req.cookies.token,
+        token_type_hint: "access_token",
+        client_id: process.env.DISCORD_CLIENT_ID as string,
+        client_secret: process.env.DISCORD_CLIENT_SECRET as string
+      }),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      }
     );
+
+    res.clearCookie("token", { httpOnly: true, secure: process.env.NODE_ENV === "production", sameSite: process.env.NODE_ENV === "production" ? "none" : "lax" });
+    res.sendStatus(200);
   } catch (error) {
     res.sendStatus(500);
   }
