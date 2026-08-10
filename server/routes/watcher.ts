c4923fc7aa0cdc260c9ed418f071e8756c3b16a6
Kenneth Ng <145921389+kennething@users.noreply.github.com>
2026-08-09 21:01:40 -0400

    delete watchers in schedules

--- server/routes/watcher.ts
+++ server/routes/watcher.ts
@@ -25,9 +25,19 @@ router.delete("/watcher/delete", authController, async (req, res) => {
 
   const [schedules, scheduleError] = tryCatch<{ term_id: string; crns: string }[]>(() => db.prepare("SELECT term_id, crns FROM schedules WHERE owner_uuid = ?").all(req.user.uuid) as any);
   if (scheduleError) return res.sendStatus(500);
-  const parsedSchedules = schedules.flatMap((schedule) => ({ ...schedule, crns: JSON.parse(schedule.crns) as string[] }));
-  if (parsedSchedules.some((schedule) => schedule.term_id === fetchedWatcher.term_id && schedule.crns.includes(fetchedWatcher.crn)))
-    return res.status(400).json({ error: "Cannot delete watcher for a class that is in a schedule" });
+
+  db.transaction(() => {
+    for (const schedule of schedules) {
+      if (schedule.term_id !== fetchedWatcher.term_id) continue;
+      const crns = JSON.parse(schedule.crns) as string[];
+
+      if (crns.includes(fetchedWatcher.crn)) {
+        const updatedCrns = crns.filter((crn) => crn !== fetchedWatcher.crn);
+        const [, updateError] = tryCatch(() => db.prepare(`UPDATE schedules SET crns = ? WHERE term_id = ? AND owner_uuid = ?`).run(JSON.stringify(updatedCrns), schedule.term_id, req.user.uuid));
+        if (updateError) return res.sendStatus(500);
+      }
+    }
+  })();
 
   const [, deleteError] = tryCatch(() => db.prepare(`DELETE FROM watchers WHERE uuid = ? AND owner_uuid = ?`).run(watcher.uuid, req.user.uuid));
   if (deleteError) return res.sendStatus(500);
