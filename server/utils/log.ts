9dad97dbead0812e6d11f35833662769592b5a22
Kenneth Ng <145921389+kennething@users.noreply.github.com>
2026-09-12 00:38:15 -0400

    format objects in log

--- server/utils/log.ts
+++ server/utils/log.ts
@@ -17,19 +17,28 @@ export class Log {
     }
   })();
 
+  private static formatMessage(message: unknown[]): string {
+    return message
+      .map((part) => {
+        if (typeof part === "object") return JSON.stringify(part, null, 2);
+        return part;
+      })
+      .join(" ");
+  }
+
   static debug(...message: unknown[]) {
-    if (Log.logLevel <= 0) console.debug(chalk.green.bold("[DEBUG] ") + chalk.green(new Date().toLocaleString()) + " | " + chalk.reset(message.join(" ")));
+    if (Log.logLevel <= 0) console.debug(chalk.green.bold("[DEBUG] ") + chalk.green(new Date().toLocaleString()) + " | " + chalk.reset(Log.formatMessage(message)));
   }
 
   static info(...message: unknown[]) {
-    if (Log.logLevel <= 1) console.info(chalk.blue.bold("[INFO] ") + chalk.blue(new Date().toLocaleString()) + " | " + chalk.reset(message.join(" ")));
+    if (Log.logLevel <= 1) console.info(chalk.blue.bold("[INFO] ") + chalk.blue(new Date().toLocaleString()) + " | " + chalk.reset(Log.formatMessage(message)));
   }
 
   static warn(...message: unknown[]) {
-    if (Log.logLevel <= 2) console.warn(chalk.yellow.bold("[WARN] ") + chalk.yellow(new Date().toLocaleString()) + " | " + chalk.reset(message.join(" ")));
+    if (Log.logLevel <= 2) console.warn(chalk.yellow.bold("[WARN] ") + chalk.yellow(new Date().toLocaleString()) + " | " + chalk.reset(Log.formatMessage(message)));
   }
 
   static error(...message: unknown[]) {
-    if (Log.logLevel <= 3) console.error(chalk.red.bold("[ERROR] ") + chalk.red(new Date().toLocaleString()) + " | " + chalk.reset(message.join(" ")));
+    if (Log.logLevel <= 3) console.error(chalk.red.bold("[ERROR] ") + chalk.red(new Date().toLocaleString()) + " | " + chalk.reset(Log.formatMessage(message)));
   }
 }
